c056672d10d948ce94b50f6e3547b5ef6e1692ba
zcill <zcillcoder@gmail.com>
2016-06-14 05:07:05 +0800

    【Feature】添加pod支持
    增加一些第三方库依赖

---
+++ Campus-Plus/Campus-Plus/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Campus-Plus
+//
+//  Created by zlk_meitu on 16/6/13.
+//  Copyright © 2016年 zcill. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
