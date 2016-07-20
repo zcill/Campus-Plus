a3ee279cb94695dec8df284259de687b021be912
zcill <zcillcoder@gmail.com>
2016-07-20 20:59:42 +0800

    【Feature】修改storyboard，在首页tableview增加head view

---
+++ Campus-Plus/Campus-Plus/AppDelegate+ZCShare.h
@@ -0,0 +1,13 @@
+//
+//  AppDelegate+ZCShare.h
+//  Campus-Plus
+//
+//  Created by zlk_meitu on 16/7/19.
+//  Copyright © 2016年 zcill. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate (ZCShare)
+
+@end
---
+++ Campus-Plus/Campus-Plus/AppDelegate+ZCShare.m
@@ -0,0 +1,13 @@
+//
+//  AppDelegate+ZCShare.m
+//  Campus-Plus
+//
+//  Created by zlk_meitu on 16/7/19.
+//  Copyright © 2016年 zcill. All rights reserved.
+//
+
+#import "AppDelegate+ZCShare.h"
+
+@implementation AppDelegate (ZCShare)
+
+@end
