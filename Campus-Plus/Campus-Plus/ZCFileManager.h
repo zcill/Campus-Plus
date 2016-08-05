f662b44296728e74784190f5db9d6375ac627ccb
zcill <zcillcoder@gmail.com>
2016-08-05 20:40:32 +0800

    【Optimize】给两个工具类的接口添加比较详细的注释

--- Campus-Plus/Campus-Plus/ZCFileManager.h
+++ Campus-Plus/Campus-Plus/ZCFileManager.h
@@ -13,10 +13,38 @@
 
 singleton_defaultInstance_interface(ZCFileManager)
 
+/**
+ *  获取沙盒Documents目录
+ *
+ *  @return Documents目录的路径
+ */
 - (NSString *)getDocumentPathInUserDomains;
+
+/**
+ *  获取沙盒Cache目录
+ *
+ *  @return Cache目录的路径
+ */
 - (NSString *)getCachePathInUserDomains;
 
+/**
+ *  移动文件
+ *
+ *  @param atPath 文件原始路径
+ *  @param toPath 移动文件的目标路径
+ *  @param error  出现错误
+ *
+ *  @return 是否移动成功
+ */
 - (BOOL)moveFileAtPath:(NSString *)atPath toPath:(NSString *)toPath error:(NSError *)error;
+
+/**
+ *  删除文件
+ *
+ *  @param error 删除错误
+ *
+ *  @return 是否删除成功
+ */
 - (BOOL)removeFilesAtCachePathIfError:(NSError *)error;
 
 @end
--- Campus-Plus/Campus-Plus/ZCJNHttpTool.h
+++ Campus-Plus/Campus-Plus/ZCJNHttpTool.h
@@ -22,17 +22,47 @@ typedef void(^failureCompletion)(ZCJxnuLoginError error);
 
 @interface ZCJNHttpTool : NSObject
 
+/**
+ *  单例
+ */
 singleton_sharedInstance_interface(ZCJNHttpTool);
 
+/**
+ *  用户是否登录
+ */
 @property (nonatomic, assign, getter=jxnu_isLogin) BOOL isLogin;     /** 是否登录 */
 
+/**
+ *  获取登录时需要传递的参数的值(默认写死)
+ *
+ *  @param stuNum 学号
+ *  @param pwd    密码
+ *
+ *  @return 参数用字典包起来返回
+ */
 - (NSMutableDictionary *)jxnu_getPostParamsWithStuNum:(NSString *)stuNum pwd:(NSString *)pwd;
 
+/**
+ *  发送登录请求
+ *  登录原理：用浏览器登录，把cookie保存，假装已经登录，登出就清除cookie
+ *
+ *  @param username    用户名/学号
+ *  @param stuPassword 密码
+ *  @param success     登录成功回调
+ *  @param failure     登录失败回调
+ */
 - (void)jxnu_loginWithStuUsername:(NSString *)username
                       stuPassword:(NSString *)stuPassword
                           success:(successCompletion)success
                           failure:(failureCompletion)failure;
 
+/**
+ *  发送登出请求
+ *  登出原理：清除cookies
+ *
+ *  @param successCompletion 登出成功回调
+ *  @param failure           登出失败回调
+ */
 - (void)jxnu_logoutWithSuccessCompletion:(void (^)())successCompletion failure:(void (^)())failure;
 
 @end
